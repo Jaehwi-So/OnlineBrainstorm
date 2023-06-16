1dbec241eeb4e06cbe6c14dd76a4a41650c8cd78
Jaehwi-So <sjh9708@gmail.com>
2023-06-17 08:54:36 +0900

    remove

--- OnlineBrainstorm/settings.py
+++ OnlineBrainstorm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-aur8#hkx4d!qxf&clstayz0l%w3090w$$eycukh+wj68&5-dpn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 # Authentication
@@ -147,7 +148,10 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
--- sample/my_django_project/tmp/blog/forms.py
+++
@@ -1,8 +0,0 @@
-from .models import Comment
-from django import forms
-
-# 사용할 폼 클래스 생성
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ('content',)   #입력
